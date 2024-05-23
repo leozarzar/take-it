128e069607e4f3e161c2b8ae00ececd6f21f8d42
Leonardo Zarzar <leomussa@icloud.com>
2024-05-23 14:04:10 -0300

    Refatorando o código

--- public/Cursor.js
+++ public/Cursor.js
@@ -1,29 +1,14 @@
-class Cursor extends Elemento{
+class Cursor {
 
-    constructor(element,x,y,id){
+    constructor(id,itsMe,x,y){
 
-        super(element);
-        this.x = x;
-        this.y = y;
-        element.hidden = false;
-        element.classList.add(`player${id}`)
-        element.style.top = `${4+20*(y-1)}px`;
-        element.style.left = `${4+20*(x-1)}px`;
-    }
-
-    mover(sentido){
-
-        if(sentido === "direita" && this.x < this.xmax) this.x++;
-        if(sentido === "esquerda" && this.x > 1) this.x--;
-        if(sentido === "cima" && this.y > 1) this.y--;
-        if(sentido === "baixo" && this.y < this.ymax) this.y++;
+        this.cursor = modeloCursor.cloneNode(true);
+        this.cursor.hidden = false;
+        this.cursor.classList.add(`player${id}`);
+        if(itsMe) this.cursor.classList.add(`this-player-cursor`);
+        this.mover(x,y);
         this.update();
-    }
-
-    update(){
-        
-        this.element.style.top = `${4+20*(this.y-1)}px`;
-        this.element.style.left = `${4+20*(this.x-1)}px`;
+        tabuleiro.appendChild(this.cursor);
     }
 
     mover(x,y){
@@ -33,4 +18,9 @@ class Cursor extends Elemento{
         this.update();
     }
 
+    update(){
+        
+        this.element.style.top = `${4+20*(this.y-1)}px`;
+        this.element.style.left = `${4+20*(this.x-1)}px`;
+    }
 }
