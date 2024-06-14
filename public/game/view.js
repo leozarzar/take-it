9fef1afab4d1a33d62f0cb9f0ce70098ac5735d8
Leonardo Zarzar <leonardozarzar@MacBook-Pro-de-Leonardo.local>
2024-06-14 06:53:39 -0300

    Adicionado pontuação, temporizador e tela de gameover.

--- public/game/view.js
+++ public/game/view.js
@@ -3,9 +3,11 @@ const tabuleiroJogadores = document.querySelector(".tabuleiro-jogadores");
 const tabuleiroAnimações = document.querySelector(".tabuleiro-animações");
 const modeloPlacarGeral = document.querySelector(".placar-geral");
 const modeloPlacar = document.querySelector(".placar");
-const modeloCursor = document.querySelector(".cursor");
-const modeloPonto = document.querySelector(".ponto");
-const modeloPontuação = document.querySelector(".pontuação");
+const pontuação = document.querySelector(".pontuação");
+const tempo = document.querySelector(".tempo");
+const telaGameOver = document.querySelector(".tela-game-over");
+const recorde = document.querySelector(".recorde");
+const resultado = document.querySelector(".resultado");
 
 const margemTabuleiro = 2;
 let larguraTabuleiro;
@@ -27,6 +29,7 @@ function view(comando,dados){
 
     const metodos = {
 
+        'criou-tabuleiro': iniciarTemporizador,
         'criou-ponto': printarPonto,
         'removeu-ponto': limparPonto,
         'animar-ponto': animarPonto,
@@ -42,6 +45,31 @@ function view(comando,dados){
 
     if(metodos[comando] !== undefined) metodos[comando](dados);
     //else console.log(`> "${comando}" não faz parte dos métodos implementados.`);
+}
+
+function iniciarTemporizador(dados){
+    
+    let temp = 120;
+    
+    let interval;
+    
+    const temporizador = () => {
+
+        if(temp === 0){
+
+            runGameOver(dados);
+            clearInterval(interval);
+        }
+
+        const tempClockCount = new Date(temp*1000);
+        const tempClock = tempClockCount.toLocaleString('pt-Br',{minute: '2-digit',second: '2-digit'});
+        tempo.innerHTML = `${tempClock}`;
+        temp--;
+    }
+
+    temporizador();
+
+    interval = setInterval(temporizador,1000)
 }
 
 function limparPonto(dados){
@@ -194,8 +222,8 @@ function animarPonto(dados){
         if(timer <= 200) alpha = alpha + 0.1;
         if(timer >= 800) alpha = alpha - 0.1;
 
-        animaçõesContext.clearRect((dados.x-1)*pixel+pixel*-0.1,60+(dados.y-1)*pixel-pixel*0.1-pixel*timer/1000+15-pixel,pixel*1.2,pixel);
-        animaçõesContext.fillText(text,(dados.x-1)*pixel+pixel*-0.1,60+(dados.y-1)*pixel-pixel*0.1-pixel*timer/1000,pixel*1.2); 
+        animaçõesContext.clearRect((dados.x-1)*pixel+pixel*-0.0,60+(dados.y-1)*pixel-pixel*0.1-pixel*timer/1000+15-pixel,pixel*1.0,pixel);
+        animaçõesContext.fillText(text,(dados.x-1)*pixel+pixel*-0.0,60+(dados.y-1)*pixel-pixel*0.1-pixel*timer/1000,pixel*1.0); 
 
         if(timer > 1000){
 
@@ -221,6 +249,7 @@ function criarPlacar(dados){
 function printarPlacar(dados){
     
     modeloPlacarGeral.querySelector(`.placar${dados.id}`).innerHTML = `${dados.nome} - ${dados.pontuação}`;
+    if(dados.meu) pontuação.innerHTML = dados.pontuação.toString().padStart(6,'0');
 }
 
 function ordenarPlacar(dados){
@@ -235,167 +264,32 @@ function ordenarPlacar(dados){
     });
 }
 
-/*
-function printarPonto(dados){
+function runGameOver(dados){
 
-    context.beginPath();
-    context.arc((dados.x-1)*pixel*1.5,(dados.y-1)*pixel*1.5,pixel*0.6,0,Math.PI*2);
-    context.fill()
+    telaGameOver.style.display = 'flex';
 
-    context.clearRect((dados.x-1)*30,(dados.y-1)*30,30,30);
+    let recordeB = localStorage.getItem('recorde') !== null ? localStorage.getItem('recorde') : '000000';
+    const resultadoB = dados.jogadores[dados.id].pontuação.toString().padStart(6,'0');
 
-    fillRoundRect({x: dados.x, y: dados.y},30,5,2);
+    if(Number.parseInt(resultadoB) > Number.parseInt(recordeB)){
 
-
-    const ponto = tabuleiro.querySelector(`.ponto${dados.id}`);
-
-    if(dados.tipo === "especial"){
-        
-        const cores = ["rgb(255, 226, 64)","rgb(255, 89, 64)","rgb(0, 247, 255)"];
-        const size = 12/20;
-
-        const interval = setInterval( () => {
-            
-            ponto.children[0].style.width = `${Math.ceil(pixel*size)}px`;
-            ponto.children[0].style.height = `${Math.ceil(pixel*size)}px`;
-            ponto.children[0].style.background = cores[0];
-            cores.push(cores.shift());
-
-        }, 100);
-
-        setTimeout( () => {
-
-            clearInterval(interval);
-
-        }, 4000)
+        recordeB = resultadoB;
+        localStorage.setItem('recorde', recordeB);
+        recorde.style.color = 'limegreen';
     }
-    else if(dados.tipo === "explosivo"){
 
-        const cores = ["#FFFFFF","#F59B89","#F42800","#F59B89"];
-        const size = [8/20,10/20,12/20,10/20];
+    recorde.innerHTML = recordeB;
+    resultado.innerHTML = resultadoB;
 
-        const interval = setInterval( () => {
+    let interval;
+    let counter = 0;
 
-            ponto.children[0].style.width = `${Math.ceil(pixel*size[0])}px`;
-            ponto.children[0].style.height = `${Math.ceil(pixel*size[0])}px`;
-            ponto.children[0].style.background = cores[0];
-            cores.push(cores.shift());
-            size.push(size.shift());
+    interval = setInterval(()=>{
 
-        }, 200);
-
-        setTimeout( () => {
-
-            clearInterval(interval);
-
-        }, 6000)
-    }
-    else{
-
-        const size = 8/20;
-        ponto.children[0].style.width = `${Math.ceil(pixel*size)}px`;
-        ponto.children[0].style.height = `${Math.ceil(pixel*size)}px`;
-        ponto.children[0].style.background = "#FFFFFF";
-    }
-    
-    ponto.style.width = `${pixel}px`;
-    ponto.style.height = `${pixel}px`;
-    ponto.style.top = `${margemTabuleiro+pixel*(dados.y-1)}px`;
-    ponto.style.left = `${margemTabuleiro+pixel*(dados.x-1)}px`;
+        counter = counter+0.05;
+        telaGameOver.style.opacity = `${counter}`;
+        if(counter == 1) clearInterval(interval);
+    },10)
 }
-
-function apagarPonto(dados){
-
-    tabuleiro.querySelector(`.ponto${dados.id}`).remove();
-}
-
-function animarPonto(dados){
-
-    const animação = modeloPontuação.cloneNode(true);
-    animação.children[0].innerHTML = `${dados.pontuação >= 0 ? "+" + dados.pontuação : dados.pontuação}`;
-    animação.children[0].hidden = false;
-    tabuleiro.appendChild(animação);
-    const color = dados.pontuação >= 0 ? "71, 255, 47" : "255, 61, 47";
-
-    let timer = 0;
-    let alpha = 0.0;
-
-    const interval = setInterval(() => {
-
-        animação.style.width = `${pixel}px`;
-        animação.style.height = `${pixel}px`;
-        animação.style.top = `${margemTabuleiro+pixel*(dados.y-1)-pixel-(timer/50)}px`;
-        animação.style.left = `${margemTabuleiro+pixel*(dados.x-1)}px`;
-
-        if(timer <= 200){
-            animação.children[0].style.color = `rgba(${color}, ${alpha})`;
-            alpha = alpha + 0.1;
-        }
-
-        if(timer >= 800){
-            animação.children[0].style.color = `rgba(${color}, ${alpha})`;
-            alpha = alpha - 0.1;
-        }
-
-        if(timer > 1000){
-
-            clearInterval(interval);
-            animação.remove();
-        }
-
-        timer = timer + 20;
-    },20);
-}
-
-function criarJogador(dados){
-
-    const jogador = modeloCursor.cloneNode(true);
-    jogador.hidden = false;
-    jogador.classList.add(`cursor${dados.id}`);
-    if(dados.meu) jogador.classList.add(`meu-cursor`);
-        
-    tabuleiro.appendChild(jogador);
-
-    printarJogador(dados);
-
-    criarPlacar(dados);
-}
-
-function printarJogador(dados){
-    
-    const jogador = tabuleiro.querySelector(`.cursor${dados.id}`);
-    jogador.style.width = `${pixel}px`;
-    jogador.style.height = `${pixel}px`;
-    jogador.style.top = `${margemTabuleiro+pixel*(dados.y-1)}px`;
-    jogador.style.left = `${margemTabuleiro+pixel*(dados.x-1)}px`;
-}
-
-function criarPlacar(dados){
-
-    const placar = modeloPlacar.cloneNode(true);
-    placar.hidden = false;
-    placar.classList.add(`placar${dados.id}`);
-    if(dados.meu) placar.classList.add(`meu-placar`);
-        
-    modeloPlacarGeral.appendChild(placar);
-
-    printarPlacar(dados);
-}
-
-function printarPlacar(dados){
-    
-    modeloPlacarGeral.querySelector(`.placar${dados.id}`).innerHTML = `${dados.nome} - ${dados.pontuação}`;
-}
-
-function ordenarPlacar(){
-    
-    tabuleiro.jogadores
-    .map((jogador)=>[jogador.pontuação,jogador.id])
-    .sort((a,b)=>b[0]-a[0])
-    .forEach((element)=>{
-        const child = modeloPlacarGeral.querySelector(`.placar${element[1]}`);
-        modeloPlacarGeral.appendChild(child);
-    });
-} */
 
 export default view;
